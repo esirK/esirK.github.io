b0570970badfdd9f70228b841f5f7c02e8701389
esir <esirkings@gmail.com>
2021-03-08 20:14:45 +0300

    Update hero section to include personalised message

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -48,12 +48,12 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Isaiah King'ori</h2>;
+  const three = <h3 className="big-heading">I build secure and extensible systems for the web</h3>;
   const four = (
     <p>
-      I'm a software engineer based in Boston, MA specializing in building (and occasionally
-      designing) exceptional websites, applications, and everything in between.
+      I'm a software engineer based in Nairobi Kenya specializing in building backend systems,
+      performing devops roles and once in a while building frontend apps.
     </p>
   );
   const five = (
