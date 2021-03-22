e0bc42a70ba2e0d3893ffbc6653bf2fc41901e55
esir <esirkings@gmail.com>
2021-03-22 20:15:17 +0300

    Adds Banking API project

---
+++ content/projects/BankingAPI.md
@@ -0,0 +1,12 @@
+---
+date: '2018-12-1'
+title: 'Banking API'
+github: 'https://github.com/esirK/BankingApi'
+external: ''
+tech:
+  - Python
+  - Django
+showInProjects: true
+---
+
+An API that simulates banking API for users like (Managers, Bank Tellers, and Customers)
--- src/config.js
+++ src/config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'isaiah.ngaruiya7@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/esirk',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: '',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/esir_kings',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/isaiah-ngaruiya/',
     },
     {
       name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: '',
     },
   ],
 
