a60544296b56abe4057a9923a1f6a19c66229658
esir <esirkings@gmail.com>
2021-03-29 16:18:23 +0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-965ffa7e3e7b44670d40.js"
+    "url": "webpack-runtime-207d98c8c058abb9981c.js"
   },
   {
     "url": "framework-305b3707783ccc9d7ca6.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f4eea6f50edbfd41770786575312f050"
+    "revision": "797e2191e02e1cf4384538c841d1c921"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
